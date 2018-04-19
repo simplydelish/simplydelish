50f1ddf9069459d7df7039f925dd0725ccb69265
Josefina Fortin <josefina.l.fortin@gmail.com>
2018-04-19 11:55:56 -1000

    Merge branch 'master' into issue-11

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import { withRouter, NavLink } from 'react-router-dom';
-import { Menu, Dropdown, Header } from 'semantic-ui-react';
+import { Menu, Dropdown, Header, Icon } from 'semantic-ui-react';
 import { Roles } from 'meteor/alanning:roles';
 
 /** The NavBar appears at the top of every page. Rendered by the App Layout component. */
@@ -16,34 +16,31 @@ class NavBar extends React.Component {
             <Header inverted as='h1'>Simply Delish</Header>
           </Menu.Item>
 
-          {/*TEST LINKS FOR M1 PAGES*/}
-          <Menu.Item as={NavLink} activeClassName="active" exact to="/userprofile" key='userprofile'>User Profile</Menu.Item>
-          <Menu.Item as={NavLink} activeClassName="active" exact to="/addvendor" key='addvendor'>Add Vendor</Menu.Item>
-          <Menu.Item as={NavLink} activeClassName="active" exact to="/toppicks" key='toppicks'>Top Picks</Menu.Item>
-          <Menu.Item as={NavLink} activeClassName="active" exact to="/vendors" key='vendors'>Vendors</Menu.Item>
-          <Menu.Item as={NavLink} activeClassName="active" exact to="/vendorhomepage" key='vendorhomepage'>Vendor Home</Menu.Item>
-          {/*TEST LINKS FOR M1 PAGES*/}
+          <Menu.Item as={NavLink} activeClassName="active" exact to="/toppicks" key='toppicks'><Icon name='star' />Top Picks</Menu.Item>
+          <Menu.Item as={NavLink} activeClassName="active" exact to="/vendors" key='list'><Icon name='map pin' />Vendors</Menu.Item>
 
 
-          {this.props.currentUser ? (
-              [<Menu.Item as={NavLink} activeClassName="active" exact to="/add" key='add'>Add Stuff</Menu.Item>,
-                <Menu.Item as={NavLink} activeClassName="active" exact to="/list" key='list'>List Stuff</Menu.Item>]
-          ) : ''}
-          {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
-              <Menu.Item as={NavLink} activeClassName="active" exact to="/admin" key='admin'>Admin</Menu.Item>
-          ) : ''}
           <Menu.Item position="right">
+            {this.props.currentUser ? (
+                <Menu.Item position="right" as={NavLink} activeClassName="active" exact to="/userprofile" key='userprofile'><Icon name='user circle' />User Profile</Menu.Item>
+            ) : ''}
+            {Roles.userIsInRole(Meteor.userId(), 'vendor') ? (
+                <Menu.Item as={NavLink} activeClassName="active" exact to="/vendorhomepage" key='vendorhomepage'><Icon name='home' />Vendor Home</Menu.Item>
+            ) : ''}
+            {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
+                <Menu.Item as={NavLink} activeClassName="active" exact to="/addvendor" key='addvendor'><Icon name='plus' />Add Vendor</Menu.Item>
+            ) : ''}
             {this.props.currentUser === '' ? (
-                <Dropdown text="Login" pointing="top right" icon={'user'}>
+                <Dropdown text="Login" pointing="top right">
                   <Dropdown.Menu>
-                    <Dropdown.Item icon="user" text="Sign In" as={NavLink} exact to="/signin"/>
-                    <Dropdown.Item icon="add user" text="Sign Up" as={NavLink} exact to="/signup"/>
+                    <Dropdown.Item text="Sign In" as={NavLink} exact to="/signin"/>
+                    <Dropdown.Item text="Sign Up" as={NavLink} exact to="/signup"/>
                   </Dropdown.Menu>
                 </Dropdown>
             ) : (
-                <Dropdown text={this.props.currentUser} pointing="top right" icon={'user'}>
+                <Dropdown text={this.props.currentUser} pointing="top right">
                   <Dropdown.Menu>
-                    <Dropdown.Item icon="sign out" text="Sign Out" as={NavLink} exact to="/signout"/>
+                    <Dropdown.Item text="Sign Out" as={NavLink} exact to="/signout"/>
                   </Dropdown.Menu>
                 </Dropdown>
             )}
