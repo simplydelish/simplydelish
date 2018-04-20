75ed500799726ba1f8762c74a23997efec3efdbc
Karissa <khuynh24@hawaii.edu>
2018-04-20 12:01:30 -1000

    Trting to implement food collections in user home-page
    Has error of other fields

--- app/imports/ui/pages/VendorHomePage.jsx
+++ app/imports/ui/pages/VendorHomePage.jsx
@@ -1,76 +1,104 @@
 import React from 'react';
-import { Grid, Header, Form, Card, Image, Button } from 'semantic-ui-react';
+import { Grid, Header, Card, Image, Segment, Button } from 'semantic-ui-react';
 import FoodCard from '../components/FoodCard';
 import PropTypes from 'prop-types';
 import { withTracker } from 'meteor/react-meteor-data';
-import { Foods } from '/imports/api/food/food';
+import { Foods, FoodSchema } from '/imports/api/food/food';
+import { Meteor } from "meteor/meteor";
+import AutoForm from 'uniforms-semantic/AutoForm';
+import TextField from 'uniforms-semantic/TextField';
+import SubmitField from 'uniforms-semantic/SubmitField';
+import HiddenField from 'uniforms-semantic/HiddenField';
+import ErrorsField from 'uniforms-semantic/ErrorsField';
+import { Bert } from 'meteor/themeteorchef:bert';
 
 /** A simple static component to render some text for the landing page. */
+
 class VendorHomePage extends React.Component {
+
+  /** Bind 'this' so that a ref to the Form can be saved in formRef and communicated between render() and submit(). */
+  constructor(props) {
+    super(props);
+    this.submit = this.submit.bind(this);
+    this.insertCallback = this.insertCallback.bind(this);
+    this.formRef = null;
+  }
+
+  /** Notify the user of the results of the submit. If successful, clear the form. */
+  insertCallback(error) {
+    if (error) {
+      Bert.alert({ type: 'danger', message: `Add failed: ${error.message}` });
+    } else {
+      Bert.alert({ type: 'success', message: 'Add succeeded' });
+      this.formRef.reset();
+    }
+  }
+
+  /** On submit, insert the data. */
+  submit(data) {
+    const { image, itemName, description } = data;
+    Foods.insert({ image, itemName, description}, this.insertCallback);
+  }
+
   render() {
     return (
-          <Grid>
-            <Grid.Column width={4} style={{ marginLeft: '150px' }}>
-              <Card>
-                <Card.Content>
-                  <Image floated='right' size='big'
-                         src='http://yoensalada.es/wp-content/uploads/2017/06/LogoPortada-1024x682-990x420.jpg'/>
-                  <Card.Header>
-                    En Saladas
-                  </Card.Header>
-                  <Card.Meta>
-                    Paradise Palms
-                  </Card.Meta>
-                  <Card.Description>
-                    Beyond the world passport dishes, En Saladas serves up the most savory selection of 6 different
-                    salads, with an option to create your own - plus unforgettable taco dishes.
-                  </Card.Description>
-                </Card.Content>
-                <Card.Content extra>
-                  <div className='ui two buttons'>
-                    <Button basic color='green'>Edit</Button>
-                    <Button basic color='red'>Delete</Button>
-                  </div>
-                </Card.Content>
-              </Card>
+        <Grid>
+          <Grid.Column width={4} style={{ marginLeft: '150px' }}>
+            <Card>
+              <Card.Content>
+                <Image floated='right' size='big'
+                       src='http://yoensalada.es/wp-content/uploads/2017/06/LogoPortada-1024x682-990x420.jpg'/>
+                <Card.Header>
+                  En Saladas
+                </Card.Header>
+                <Card.Meta>
+                  Paradise Palms
+                </Card.Meta>
+                <Card.Description>
+                  Beyond the world passport dishes, En Saladas serves up the most savory selection of 6 different
+                  salads, with an option to create your own - plus unforgettable taco dishes.
+                </Card.Description>
+              </Card.Content>
+              <Card.Content extra>
+                <div className='ui two buttons'>
+                  <Button basic color='green'>Edit</Button>
+                  <Button basic color='red'>Delete</Button>
+                </div>
+              </Card.Content>
+            </Card>
+          </Grid.Column>
 
-            </Grid.Column>
+          <Grid.Column width={9}>
+            <Header as="h2" textAlign="center">Add Item</Header>
 
-            <Grid.Column width={9}>
+            <AutoForm ref={(ref) => { this.formRef = ref;}} schema={FoodSchema} onSubmit={this.submit}>
 
-              <Header as="h2" textAlign="center">Add Item</Header>
-              <Form>
-                <Form.Group widths='equal'>
-                  <Form.Input label='Menu Item' placeholder='Menu Item'/>
-                </Form.Group>
-                <Form.TextArea label='Description' placeholder='Tell us more about this item...'/>
-                <Form.Group widths='equal'>
-                  <Form.Input label='Image' placeholder='Give us an image...'/>
-                </Form.Group>
-                <Form.Button>Add Item</Form.Button>
-              </Form>
-            </Grid.Column>
+              <Segment>
+                <TextField name='itemName' placeholder='Menu Item'/>
+                <TextField name='description' placeholder='Tell us more about this item...'/>
+                <TextField name='image' placeholder='Give us an image...'/>
+                <SubmitField value='Add Item'/>
+                <ErrorsField/>
+              </Segment>
+            </AutoForm>
+          </Grid.Column>
 
-            <Grid.Row>
-
-                  {this.props.foods.map((vendor) =>
-                      <FoodCard
-                          key={vendor._id}
-                          image={vendor.image}
-                          itemName={vendor.itemName}
-                          vendorName={vendor.vendorName}
-                          description={vendor.description}
-                          numReviews={vendor.numReviews}
-                          numLikes={vendor.numLikes}
-                      />)}
-
-
-            </Grid.Row>
-          </Grid>
+          <Grid.Row>
+            {this.props.foods.map((vendor) =>
+                <FoodCard
+                    key={vendor._id}
+                    image={vendor.image}
+                    itemName={vendor.itemName}
+                    vendorName={vendor.vendorName}
+                    description={vendor.description}
+                    numReviews={vendor.numReviews}
+                    numLikes={vendor.numLikes}
+                />)}
+          </Grid.Row>
+        </Grid>
     );
   }
 }
-
 
 VendorHomePage.propTypes = {
   foods: PropTypes.array.isRequired,
